cae7a87ca3fdb2a7dfdb578a7612be4f6a175b45
Donghyun Kim <kdh949@naver.com>
2026-03-07 21:14:00 +0900

    #17 - 최댓값 closed

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,14 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+max_val = 0
+max_idx = 0
+
+for i in range(9):
+    num = int(input())
+    if(max_val < num):
+        max_val = num
+        max_idx = i + 1
+    
+print(max_val)
+print(max_idx)
