9023b7313af043a42ee8a6623c7694fab1fc0365
Donghyun Kim <kdh949@naver.com>
2026-03-07 21:40:28 +0900

    #18 - 배열 - 평균은 넘겠지 Done

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,23 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+def calc_percentile():
+    score_list = list(map(int, input().split()))
+    total_stu = score_list[0]
+    
+    score_sum = sum(score_list) - total_stu
+
+    avg = score_sum / float(total_stu)
+    cnt = 0
+    
+    for i in range(1, len(score_list)):
+        if score_list[i] > avg:
+            cnt += 1
+    
+    return cnt / float(total_stu)
+
+n = int(input())
+
+for _ in range(n):
+    result = round(calc_percentile() * 100, 3)
+    print(f"{result} %")
