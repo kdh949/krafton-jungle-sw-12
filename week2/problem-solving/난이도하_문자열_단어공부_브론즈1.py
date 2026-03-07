393f45b70a9cfdfd25efa2d26e240b3304dcdc56
Donghyun Kim <kdh949@naver.com>
2026-03-07 22:39:34 +0900

    문자열 - 단어 공부 #20

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,27 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+str = input().upper()
+cnt = [0]*26
+
+for c in str:
+    cnt[ord(c)-65] += 1
+
+max_idx = 0
+max_val = 0
+nxt_idx = 0
+nxt_val = 0
+
+
+for i in range(26):
+    if (max_val < cnt[i]):
+        max_idx = i
+        max_val = cnt[i]
+    elif (nxt_val < cnt[i]):
+        nxt_idx = i
+        nxt_val = cnt[i]
+        
+if (max_val == nxt_val):
+    print("?")
+else:
+    print(chr(max_idx+65))
