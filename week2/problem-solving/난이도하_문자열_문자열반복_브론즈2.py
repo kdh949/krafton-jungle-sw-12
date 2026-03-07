6502fd12fd663c2060471f5ede603dceae6c2445
Donghyun Kim <kdh949@naver.com>
2026-03-07 22:16:20 +0900

    백준 문자열 반복 - close #19

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,16 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+test_case = int(input())
+
+for _ in range(test_case):
+    n, str = input().split(" ")
+    n = int(n)
+    
+    result = ""
+    for c in str:
+        for _ in range(n):
+            result+=c
+    
+    
+    print(result)
