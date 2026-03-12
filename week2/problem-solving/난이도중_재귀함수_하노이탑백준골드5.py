d4896c65a0eb0af8b09cacc45ef3ffbf016302b8
Donghyun Kim <kdh949@naver.com>
2026-03-12 23:21:25 +0900

    #25 - 하노이탑

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,43 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+cnt = 0
+move_list = []
+
+def hanoi(n, start, aux, end):
+    global cnt
+    # cnt += 1
+    
+    # if(n == 1):
+    #     return
+    
+    # hanoi(n-1, start, end, aux)
+    # hanoi(n-1, aux, start, end)
+    
+    for i in range(n):
+        cnt += 2**i
+    
+def hanoi_under(n, start, aux, end):
+    global cnt
+    cnt += 1
+    
+    if(n == 1):
+        # start -> end
+        move_list.append([start, end])
+        return
+    
+    hanoi_under(n-1, start, end, aux)
+    move_list.append([start, end])
+    hanoi_under(n-1, aux, start, end)
+
+    
+n = int(input())
+
+if (n > 20):  
+    hanoi(n, 1, 2, 3)
+    print(cnt)
+else:
+    hanoi_under(n, 1, 2, 3)
+    print(cnt)
+    for arr in move_list:
+        print(f"{arr[0]} {arr[1]}")
