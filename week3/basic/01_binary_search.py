84bfa4f0977448fe3e641b66851d9e9c4ffa2d9f
Donghyun Kim <kdh949@naver.com>
2026-03-13 16:10:50 +0900

    done #43 - 이진탐색 기초

--- week3/basic/01_binary_search.py
+++ week3/basic/01_binary_search.py
@@ -33,16 +33,27 @@ def binary_search(arr, target):
     Returns:
         target의 인덱스 (없으면 -1)
     """
+    
+    ## 인덱스
     left = 0
     right = len(arr) - 1
     
-    # TODO: left가 right보다 작거나 같을 때까지 반복
+    # TODO: left가 right보다 작거나 같을 때까지 반복 = left가 right보다 커지면 stop
     ## 중간 인덱스 계산
     ## arr[mid]와 target 비교
     ## 같으면 mid 반환
     ## target이 더 크면 left = mid + 1
     ## target이 더 작으면 right = mid - 1
-    pass
+    while(left <= right):
+        mid = (left + right) // 2
+        
+        if(arr[mid] == target):
+            return mid
+        
+        if(arr[mid] > target):
+            right = mid -1
+        else:
+            left = mid + 1
     
     return -1
 
