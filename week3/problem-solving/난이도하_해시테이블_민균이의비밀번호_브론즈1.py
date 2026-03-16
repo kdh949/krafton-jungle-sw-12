650f0b39150665baeb3d6a3231aa1454583e23a8
Donghyun Kim <kdh949@naver.com>
2026-03-16 21:25:11 +0900

    close #57 - 민균이의 비밀번호 (백준)

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,15 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+n = int(input())
+pass_set = set()
+
+for i in range(n):
+    password = input()
+    pass_set.add(password)
+
+    if password[::-1] in pass_set:
+        pass_len = len(password)
+        print(pass_len, end=' ')
+        print(password[pass_len//2])
+        break
