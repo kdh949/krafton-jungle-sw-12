3f7756740245b0cb988f54a30c49009f9f7bdd82
Donghyun Kim <kdh949@naver.com>
2026-03-21 17:29:15 +0900

    #84 - 상근이의 여행

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,12 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    for i in range(m):
+        input()
+    print(n-1)
