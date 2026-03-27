54920311ba56841629980c4b46a1ac9fb71a33d0
Donghyun Kim <kdh949@naver.com>
2026-03-28 00:20:17 +0900

    close #113 - 피보나치수2

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,18 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+
+if n <= 2:
+    print(n)
+    exit()
+
+fib = [-1] * (n+1)
+
+fib[0] = 0
+fib[1] = 1
+
+for i in range(2, n+1):
+    fib[i] = fib[i-1]+fib[i-2]
+    
+print(fib[n])
