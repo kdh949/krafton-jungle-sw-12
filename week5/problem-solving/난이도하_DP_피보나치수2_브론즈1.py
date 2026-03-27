4ce40397315eea0684ff35d49dbfafbf6248ba66
Donghyun Kim <kdh949@naver.com>
2026-03-28 00:21:33 +0900

    #113 - 피보나치수 (수정)

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -3,7 +3,7 @@
 
 n = int(input())
 
-if n <= 2:
+if n <= 1:
     print(n)
     exit()
 
@@ -13,6 +13,6 @@ fib[0] = 0
 fib[1] = 1
 
 for i in range(2, n+1):
-    fib[i] = fib[i-1]+fib[i-2]
+    fib[i] = fib[i-1] + fib[i-2]
     
 print(fib[n])
