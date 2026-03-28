3e51bf1b73d4a9eb1517d01fefe9f052edb8a597
Donghyun Kim <kdh949@naver.com>
2026-03-28 15:31:49 +0900

    close #116 - 잃어버린 괄호

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,15 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+eq = list(input().split("-"))
+add = 0
+minus = 0
+
+for val in list(eq[0].split('+')):
+    add += int(val)
+    
+for i in range(1, len(eq)):
+    for minus_val in list(eq[i].split("+")):
+        minus += int(minus_val)
+
+print(add - minus)
