0f387ab4bc09aca0c84dee508aa97b158984bd34
Donghyun Kim <kdh949@naver.com>
2026-03-29 17:08:23 +0900

    close #115 - 그리디 - 동전 0

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,21 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+n, k = map(int, input().split())
+
+coins = [0]*n
+
+for i in range(n):
+    coins[i] = int(input())
+
+result = 0
+left_coins = k
+
+for j in range(n-1, -1, -1):
+    result += left_coins // coins[j]
+    left_coins %= coins[j]
+    
+    if left_coins == 0:
+        break
+
+print(result)
