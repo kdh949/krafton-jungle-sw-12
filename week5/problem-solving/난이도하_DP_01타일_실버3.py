21fef28a28b565eb00491213f1b65a4da07710d6
Donghyun Kim <kdh949@naver.com>
2026-03-29 16:50:38 +0900

    clsoe #114 - DP - 01타일

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,15 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+curr_tile = 1
+result = 0
+
+for _ in range(n+1):
+    curr_tile, result = result, (curr_tile+result)%15746
+
+print(result)
