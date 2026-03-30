2d964da7be541e7b0338833e27d3f32b76ba759c
Donghyun Kim <kdh949@naver.com>
2026-03-30 11:35:22 +0900

    close #117 - DP - 동전

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,21 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+def combination_coins(n, m, coins):
+    dp = [0]*(m+1)
+    dp[0] = 1 # 아무것도 사용하지 않는 케이스도 경우의 수
+    
+    for coin in coins:
+        for j in range(coin, m+1):
+            dp[j] = dp[j] + dp[j-coin]
+
+    return dp[m]
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+    
+    print(combination_coins(n, m, coins))
